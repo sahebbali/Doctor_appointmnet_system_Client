7c4d9e8fac00ebf40dbc2c70a95a077f9d253760
sahebbali <sahebbali253@gmail.comm>
2023-07-21 16:24:35 +0600

    add docotor and user

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import { useSelector } from "react-redux";
 import Spinner from "./components/Spinner";
 import ApplyDoctor from "./pages/ApplyDoctor";
 import NotificationPage from "./pages/NotificationPage";
+import Users from "./pages/admin/Users";
+import Doctors from "./pages/admin/Doctors";
 
 function App() {
   const  loading  = useSelector((state) => state.alerts.loading);
@@ -22,6 +24,8 @@ function App() {
            <Route path="/" element={<ProtectedRoute> <HomePage /> </ProtectedRoute>} />
            <Route path="/apply-doctor" element={<ProtectedRoute> <ApplyDoctor /> </ProtectedRoute>} />
            <Route path="/notification" element={<ProtectedRoute> <NotificationPage /> </ProtectedRoute> } />
+           <Route path="/admin/users" element={ <ProtectedRoute> <Users /> </ProtectedRoute>}/>
+            <Route path="/admin/doctors" element={ <ProtectedRoute> <Doctors /> </ProtectedRoute> } />
            <Route path="/login" element={ <PublicRoute> <Login /> </PublicRoute> } />
            <Route path="/register" element={ <PublicRoute> <Register /> </PublicRoute>} />
          </Routes>
