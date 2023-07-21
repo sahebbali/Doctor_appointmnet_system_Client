5281d40cf1ed163bfff52ac6e797326fbecd955f
sahebbali <sahebbali253@gmail.comm>
2023-07-21 15:38:23 +0600

    add notification

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import PublicRoute from './components/PublicRoute'
 import ProtectedRoute from './components/ProtectedRoute'
 import { useSelector } from "react-redux";
 import Spinner from "./components/Spinner";
+import ApplyDoctor from "./pages/ApplyDoctor";
+import NotificationPage from "./pages/NotificationPage";
 
 function App() {
   const  loading  = useSelector((state) => state.alerts.loading);
@@ -18,6 +20,8 @@ function App() {
         ) : (
            <Routes>
            <Route path="/" element={<ProtectedRoute> <HomePage /> </ProtectedRoute>} />
+           <Route path="/apply-doctor" element={<ProtectedRoute> <ApplyDoctor /> </ProtectedRoute>} />
+           <Route path="/notification" element={<ProtectedRoute> <NotificationPage /> </ProtectedRoute> } />
            <Route path="/login" element={ <PublicRoute> <Login /> </PublicRoute> } />
            <Route path="/register" element={ <PublicRoute> <Register /> </PublicRoute>} />
          </Routes>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,39 +1,72 @@
-import React from 'react'
-import '../styles/LayoutStyles.css'
-import { SidebarMenu } from '../Data/data'
-import { Link, useLocation } from 'react-router-dom'
+import React from "react";
+import "../styles/LayoutStyles.css";
+import { adminMenu, userMenu } from "./../Data/data";
 
-const Layout = ({children }) => {
-    const location = useLocation();
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Badge, message } from "antd";
+const Layout = ({ children }) => {
+  const { user } = useSelector((state) => state.user);
+  const location = useLocation();
+  const navigate = useNavigate();
+  // logout funtion
+  console.log(user);
+  const handleLogout = () => {
+    localStorage.clear();
+    message.success("Logout Successfully");
+    navigate("/login");
+  };
+
+  // redering menu list
+  const SidebarMenu = user?.isAdmin ? adminMenu : userMenu;
   return (
-    <div className='main'>
+    <>
+      <div className="main">
         <div className="layout">
-            <div className="sidebar">
-                <div className="logo">
-                    <h3>Doc APP</h3>
-                    <hr />
-                </div>
-                <div className="menu">
-                    {SidebarMenu.map((menu)=>{
-                        const isActive = location.pathname === menu.path;
-                        return(
-                            <>
-                            <div className={`menu-item ${isActive && "active"}`}>
-                                <l className={menu.name}></l>
-                                <Link to={menu.path}>{menu.name}</Link>
-                            </div>
-                            </>
-                        );
-                    })}
-                </div>
+          <div className="sidebar">
+            <div className="logo">
+              <h6>DOC APP</h6>
+              <hr />
             </div>
-            <div className="content">
-                <div className='header'>Header Content</div>
-                <div className="body">{children}</div>
+            <div className="menu">
+              {SidebarMenu.map((menu) => {
+                const isActive = location.pathname === menu.path;
+                return (
+                  <>
+                    <div className={`menu-item ${isActive && "active"}`}>
+                      <i className={menu.icon}></i>
+                      <Link to={menu.path}>{menu.name}</Link>
+                    </div>
+                  </>
+                );
+              })}
+              <div className={`menu-item `} onClick={handleLogout}>
+                <i className="fa-solid fa-right-from-bracket"></i>
+                <Link to="/login">Logout</Link>
+              </div>
             </div>
-        </div>
-    </div>
-  )
-}
+          </div>
+          <div className="content">
+            <div className="header">
+            <div className="header-content" style={{ cursor: "pointer" }}>
+                <Badge
+                  count={user && user.notifcation?.length}
+                  onClick={() => {
+                    navigate("/notification");
+                  }}
+                >
+                  <i class="fa-solid fa-bell"></i>
+                </Badge>
 
-export default Layout
+                <Link to="/profile">{user?.name}</Link>
+              </div>
+            </div>
+            <div className="body">{children}</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
